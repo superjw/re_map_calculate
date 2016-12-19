de1e8d2f770f1c327520a6e0260fdfd577aeefa4
J  WANG <jw.wang@live.com>
2016-12-19 11:54:03 +0000

    first commit

--- split_snps_by_chr_number.py
+++ split_snps_by_chr_number.py
@@ -3,8 +3,18 @@
 #     print(i)
 #     outfile = open('chr' + str(i) + 'tsv', 'w')
 import sys
+# def build_dict_for_chr(chr_number):
+#     """
+#     used for building a dictionary of gene location on chromosome "chr_number"
+#     :param chr_number: chormosome number
+#     :return:
+#     """
+#     gene_location_file_name = 'pos.chr' + chr_number + '.gene.tsv'
+#     with open("'../gene_location/' + gene_location_file_name", 'r') as f:
 
-out_file_name = 'chr' + str(sys.argv[1]) + '.tsv'
+
+# sys.argv[1] used to determin chromosome number
+out_file_name = './batchfile/chr' + str(sys.argv[1]) + '.tsv'
 outfile = open(out_file_name, 'w')
 with open('GWAS_snp_positions_on_grch37_autosome_remove_patch.tsv', 'r') as f:
     header = next(f)
@@ -20,3 +30,4 @@ with open('GWAS_snp_positions_on_grch37_autosome_remove_patch.tsv', 'r') as f:
 
 
 print('split snps by chromosome number done for chromosome ' + str(sys.argv[1]) + '!')
+
