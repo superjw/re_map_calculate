ef7ee95d0de3b27e56c567a58bf3e63a01585c02
J  WANG <jw.wang@live.com>
2016-12-20 11:25:49 +0000

    mapped genes were added to GWAS-Catalog file

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 * `GWAS_snp_positions_on_grch37_autosome_remove_patch.tsv` as shown in the file name, is a list of positions for snps mapped to autosome main assembly only
 * `entries_standard_rs_id.tsv` GWAS-catalog entries with standard rs id reported SNPs.
 * `entries_non_rs_id.tsv` GWAS-catalog entries with non-standard rs id reported SNPs.
+* `GWAS_entries_grch37_remapping.tsv` is the final file for GWAS-Catalog SNPs mapping to GRCh37
+* `add_remapped_gene_to_standard_entries.py**` is the script used for add mapped genes to GWAS-Catalog SNPs ---- generated `GWAS_entries_grch37_remapping.tsv`
--- add_remapped_gene_to_standard_entries.py
+++ add_remapped_gene_to_standard_entries.py
@@ -7,14 +7,18 @@ def build_dict():
     :return:
     """
     d = {}
-    for i in range(1,23):
-        file_name = './batch_file/gwascatalog_chr' + str(i) +'_snp_mapping_1000_flank_grch37.tsv'
+    for i in range(1, 23):
+        file_name = './batch_file/gwascatalog_chr' + str(i) + '_snp_mapping_1000_flank_grch37.tsv'
         with open(file_name, 'r') as f:
             for line in f:
                 line_split = line.strip().split('\t')
                 if len(line_split) > 2:
                     d[line_split[0]] = line_split[2]
     return d
+
+
+def xstr(s):
+    return '' if s is None else str(s)
 
 
 def get_mapped_gene_based_on_rs(one_or_multi_rs_id, rs_gene_dictionary):
@@ -29,22 +33,29 @@ def get_mapped_gene_based_on_rs(one_or_multi_rs_id, rs_gene_dictionary):
     elif len(multiple_rs) > 1:
         mapped_gene_list = []
         for k in multiple_rs:
-            mapped_gene_list.append(rs_gene_dictionary.get(k))
+            if rs_gene_dictionary.get(k):
+                mapped_gene_list.append(rs_gene_dictionary.get(k))
+            else:
+                print('no value found!')
+                pass
+            # print(mapped_gene_list)
+            # if not mapped_gene_list:
             mapped_gene_string = ','.join(mapped_gene_list)
-            print(mapped_gene_string)
+            #     print(mapped_gene_string)
     else:
         print('error detected!')
     return mapped_gene_string
 
 
 outfile = open('GWAS_entries_grch37_remapping.tsv', 'w')
-with open('./entries_standard_rs_id.tsv', 'r') as f:
+with open('./entries_standard_rs_id.tsv', 'r', encoding='utf8') as f:
     header = next(f)
     outfile.write(header.strip() + '\t' + 'remapped_gene' + '\n')
     dictionary = build_dict()
     for line in f:
         rs = line.strip().split('\t')[7]
         mapped_gene = get_mapped_gene_based_on_rs(rs, dictionary)
-        outfile.write(line.strip() + '\t{}\n'.format(mapped_gene))
+        mapped_gene = xstr(mapped_gene)
+        outfile.write(line.strip() + '\t' + mapped_gene + '\n')
 outfile.close()
 print('remapping done!')
